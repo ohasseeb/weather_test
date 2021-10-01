578dc477888f6af4dcecd296cf3b20cf72e02842
obaidmaster <ohasseeb@ucdavis.edu>
2021-10-01 16:06:14 -0700

    Searching Cities work, need to work on edge cases then styling

--- src/App.js
+++ src/App.js
@@ -6,25 +6,62 @@ class App extends React.Component {
 
     this.state = {
       totalReactPackages: [],
+      cityinput: " ",
     };
   }
 
-  componentDidMount() {
+  componentDidMount(props) {
     // Simple GET request using fetch
-    fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=alameda&appid=5641904b9f9d3a2b1751db682fa354ff"
-    )
+    // fetch(
+    //   "https://api.openweathermap.org/data/2.5/weather?q=alameda&appid=5641904b9f9d3a2b1751db682fa354ff&units=imperial"
+    // )
+    //   .then((response) => response.json())
+    //   .then((data) => this.setState({ totalReactPackages: data }));
+  }
+
+  handleChange = (event) => {
+    // console.log(event.target.value);
+    this.setState({ cityinput: event.target.value });
+  };
+  handleSubmit = (event) => {
+    console.log("In the function");
+    console.log(event.target.value);
+    console.log(this.state.cityinput);
+    this.searchForTheWeather();
+    //this.setState({ cityInput: event.target.value });
+  };
+
+  searchForTheWeather = () => {
+    var APPID = this.state.cityinput;
+    console.log(this.state.cityinput);
+    var weather_url =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      APPID +
+      "&appid=5641904b9f9d3a2b1751db682fa354ff&units=imperial";
+
+    fetch(weather_url)
       .then((response) => response.json())
       .then((data) => this.setState({ totalReactPackages: data }));
-  }
+  }; // End SEarch for the weather
 
   render() {
     const { totalReactPackages } = this.state;
     return (
       <div className="card text-center m-3">
-        <h5 className="card-header">Simple GET Request</h5>
+        <label htmlFor="WeatherInput"> City </label>
+        <input
+          id="WeatherInput"
+          type="text"
+          placeholder="Alameda "
+          onChange={this.handleChange}
+        />
+
+        <button value onClick={this.handleSubmit}>
+          Search
+        </button>
+
+        <h5 className="card-header">Daily Weather</h5>
         <div className="card-body">
-          Total react packages: {(totalReactPackages.length, "\n")}
           <br />
           City: {totalReactPackages.name ? totalReactPackages.name : " "}
           <br />
@@ -33,6 +70,12 @@ class App extends React.Component {
           Feels Like:{""}
           {totalReactPackages.main ? totalReactPackages.main.feels_like : " "}
           {console.log(totalReactPackages)}
+          <br />
+          Description:{""}
+          {totalReactPackages.main ? totalReactPackages.weather[0].main : " "}
+          {console.log(
+            totalReactPackages.main ? totalReactPackages.weather[0].main : " "
+          )}
         </div>
       </div>
     );
